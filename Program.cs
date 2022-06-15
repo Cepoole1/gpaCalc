43b070a0e10ef93efb5ae1330480e1d919786bd4
chandler <cepoole1@crimson.ua.edu>
2022-06-15 12:55:56 -0500

    GPACALC

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+//GPA Calc
+//Prompt user for total points
+//Prompt user for total hours
+//Display GPA
+
+//String
+String FirstName;
+Console.WriteLine("Enter in your first name");
+FirstName = Console.ReadLine();
+
+
+//double
+double totalPoints = 0.0;
+Console.WriteLine("Enter in your total points");
+totalPoints = double.Parse(Console.ReadLine());
+
+double totalHours = 0.0;
+Console.WriteLine("Enter in your total hours");
+totalHours = double.Parse(Console.ReadLine());
+
+double GPA;
+
+
+//Process
+GPA = totalPoints/totalHours;
+
+//Output
+Console.WriteLine(FirstName +'\t'+ GPA);
+
+
